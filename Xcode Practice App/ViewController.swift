cf0e212dad38528d5cc228409f91ad0cee530519
Kenneth Uyabeme <uyabeme@oa-s231-13.ddi.sheridanc.on.ca>
2018-09-28 12:42:18 -0400

    Initial Commit

---
+++ Xcode Practice App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode Practice App
+//
+//  Created by Kenneth Uyabeme on 9/28/18.
+//  Copyright © 2018 Kenneth Uyabeme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
