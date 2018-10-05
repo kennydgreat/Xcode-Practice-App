7e7d608e10ea61835874ad7f881d16bb296ec3b9
Kenneth Uyabeme <uyabeme@oa-s231-13.ddi.sheridanc.on.ca>
2018-10-05 14:31:46 -0400

    -Table View Lecture

---
+++ Xcode Practice App/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Xcode Practice App
+//
+//  Created by Kenneth Uyabeme on 10/5/18.
+//  Copyright © 2018 Kenneth Uyabeme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TableViewController : NSObject
+
+@end
---
+++ Xcode Practice App/TableViewController.m
@@ -0,0 +1,13 @@
+//
+//  TableViewController.m
+//  Xcode Practice App
+//
+//  Created by Kenneth Uyabeme on 10/5/18.
+//  Copyright © 2018 Kenneth Uyabeme. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@implementation TableViewController
+
+@end
